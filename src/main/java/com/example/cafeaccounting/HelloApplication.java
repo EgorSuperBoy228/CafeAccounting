573a8078d78b582dd277f4bf2fa22cbca183abdb
Егор Гординский <yegorgordinskiy2002@gmail.com>
2022-04-25 22:42:24 +0800

    Проверка

--- src/main/java/com/example/cafeaccounting/HelloApplication.java
+++ src/main/java/com/example/cafeaccounting/HelloApplication.java
@@ -16,22 +16,6 @@ import javafx.stage.StageStyle;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-   // class WindowButtons extends HBox {
-
-        /*public WindowButtons() {
-            Button closeBtn = new Button("X");
-
-            closeBtn.setOnAction(new EventHandler<ActionEvent>() {
-
-                @Override
-                public void handle(ActionEvent actionEvent) {
-                    Platform.exit();
-                }
-            });
-
-            this.getChildren().add(closeBtn);
-        }
-    }*/
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("mainwindow-view.fxml"));
@@ -40,23 +24,6 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
-
-
-        //BorderPane borderPane = new BorderPane();
-        //borderPane.setStyle("-fx-background-color: green;");
-
-        /*ToolBar toolBar = new ToolBar();
-
-        int height = 25;
-        toolBar.setPrefHeight(height);
-        toolBar.setMinHeight(height);
-        toolBar.setMaxHeight(height);
-        //toolBar.getItems().add(new WindowButtons());
-
-        borderPane.setTop(toolBar);*/
-
-        //stage.setScene(new Scene(borderPane, 736, 468));
-        //stage.show();
     }
 
     public static void main(String[] args) {
